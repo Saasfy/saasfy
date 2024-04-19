a38f212d7e3adffe141d44b5ab99156887eae0c6
Igor Katsuba <igor@katsuba.dev>
2024-04-19 23:11:07 +0300

    feat: update user name

--- apps/web/app/app/(user)/settings/page.tsx
+++ apps/web/app/app/(user)/settings/page.tsx
@@ -8,22 +8,57 @@ import {
   CardTitle,
 } from '@saasfy/ui/card';
 import { Input } from '@saasfy/ui/input';
+import { createAdminClient, getUser } from '@saasfy/supabase/server';
+import { revalidatePath } from 'next/cache';
 
-export default function Settings() {
+async function updateName(formData: FormData) {
+  'use server';
+
+  const user = await getUser();
+
+  if (!user) {
+    return;
+  }
+
+  const name = formData.get('name');
+
+  await createAdminClient().auth.admin.updateUserById(user.id, {
+    user_metadata: {
+      name,
+    },
+  });
+
+  revalidatePath('/settings');
+}
+
+export default async function Settings() {
+  const user = await getUser();
+
+  if (!user) {
+    return (
+      <div>
+        <h1>Not logged in</h1>
+        <p>You need to be logged in to access this page.</p>
+      </div>
+    );
+  }
+
   return (
     <>
       <Card>
         <CardHeader>
-          <CardTitle>Username</CardTitle>
-          <CardDescription>The username of the account owner.</CardDescription>
+          <CardTitle>Name</CardTitle>
+          <CardDescription>The name of the account owner.</CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <Input placeholder="Store Name" />
+          <form id="nameForm">
+            <Input placeholder="Store Name" name="name" defaultValue={user.user_metadata.name} />
           </form>
         </CardContent>
         <CardFooter className="border-t px-6 py-4">
-          <Button>Save</Button>
+          <Button type="submit" form="nameForm" formAction={updateName}>
+            Save
+          </Button>
         </CardFooter>
       </Card>
     </>
