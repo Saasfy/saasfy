fb9f0138bb153b6f2cde0ac3c46f4cf6d0d38750
Igor Katsuba <igor@katsuba.dev>
2024-05-06 21:36:37 +0300

    fix(web): workspace list is available only for workspace user

--- apps/web/app/app/(dashboard)/layout.tsx
+++ apps/web/app/app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode, Suspense } from 'react';
-import { headers } from 'next/headers';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
@@ -19,7 +18,7 @@ export default async function DashboardLayout({ children }: { children: ReactNod
 
   const supabase = createAdminClient();
 
-  const { data: workspaces } = await supabase.from('workspaces').select('*');
+  const { data: workspaces } = await supabase.from('workspaces').select('*').eq('user_id', user.id);
 
   return (
     <div>
