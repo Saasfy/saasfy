118d1a208f672d92ccd559bb6f5fe9ec6f06cc72
Igor Katsuba <igor@katsuba.dev>
2024-05-09 00:34:21 +0300

    feat(emails): add email preview
    Close #37

---
+++ packages/emails/src/lib/my-email.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import { Button, Html } from '@react-email/components';
+
+export function MyEmail() {
+  return (
+    <Html>
+      <Button
+        href="https://example.com"
+        style={{ background: '#000', color: '#fff', padding: '12px 20px' }}
+      >
+        Click me
+      </Button>
+    </Html>
+  );
+}
+
+export default MyEmail;
